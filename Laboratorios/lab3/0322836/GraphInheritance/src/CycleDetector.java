b873a32d8e8faac229cccc2276841226f2fcdd14
franzejr@gmail.com <franzejr@gmail.com@69e311a5-d971-8743-4961-0525734a4e8e>
2011-05-25 23:26:14 +0000

    Colocando o começo do detector de Ciclo

--- Laboratorios/lab3/0322836/GraphInheritance/src/CycleDetector.java
+++ Laboratorios/lab3/0322836/GraphInheritance/src/CycleDetector.java
@@ -1,4 +1,34 @@
+import java.util.Stack;
+
 
 public class CycleDetector {
+	
+	Stack<Vertex> stack;
+	
+	public boolean has(){
+		
+		while(hasWhiteVertex()){
+			
+		}
+		
+	}
+	
+	public void paintAllWhite(){
+		
+	}
+	
+	public boolean hasWhiteVertex(){
+		
+	}
+	
+	public boolean hasGrayVertex(){
+		
+	}
+	
+	public boolean hasWhiteNeighbor(){
+		
+	}
+	
+	
 
 }
